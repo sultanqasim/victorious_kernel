d60661b5b6162b029814ad172f317d372f18e9c1
Eric W. Biederman <ebiederm@xmission.com>
2012-07-26 21:42:03 -0700

    vfs: Allow unprivileged manipulation of the mount namespace.
    - Add a filesystem flag to mark filesystems that are safe to mount as
      an unprivileged user.
    
    - Add a filesystem flag to mark filesystems that don't need MNT_NODEV
      when mounted by an unprivileged user.
    
    - Relax the permission checks to allow unprivileged users that have
      CAP_SYS_ADMIN permissions in the user namespace referred to by the
      current mount namespace to be allowed to mount, unmount, and move
      filesystems.
    
    Acked-by: "Serge E. Hallyn" <serge@hallyn.com>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    (cherry picked from commit 0c55cfc4166d9a0f38de779bd4d75a90afbe7734)
    
    Conflicts:
    	include/linux/fs.h

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1857,7 +1857,7 @@ int sync_inode_metadata(struct inode *inode, int wait);
 struct file_system_type {
 	const char *name;
 	int fs_flags;
-#define FS_REQUIRES_DEV	1 
+#define FS_REQUIRES_DEV		1 
 #define FS_BINARY_MOUNTDATA	2
 #define FS_HAS_SUBTYPE		4
 #define FS_USERNS_MOUNT		8	/* Can be mounted by userns root */
